88c6e1f2da42c09d88898480c3c6d56c9364dbf5
bhaskarmelkani <melkani.bhaskar@gmail.com>
2017-07-05 16:38:50 +0200

    chore(lint): Add es lint to the project

--- packages/crawling-test/index.js
+++ packages/crawling-test/index.js
@@ -1,8 +1,10 @@
+'use strict';
+
 const Crawler = require('js-crawler');
 
 const crawl = (options) => {
   const crawler = new Crawler().configure({
-    shouldCrawl: function(url) {
+    shouldCrawl: function (url) {
       // just "internal" urls
       return url.indexOf(options.url) > -1;
     }
@@ -14,19 +16,22 @@ const crawl = (options) => {
     crawler.crawl({
       url: options.url,
       success: function (page) {
-        console.log(`success - url: ${page.url}, referer: ${page.referer}, status code: ${page.status}`);
+        console.log(`success - url: ${page.url}, referer: ${page.referer}, status code: ${page.status}`);  // eslint-disable-line no-console
+
         succeded.push(page);
       },
       failure: function (page) {
-        console.error(`failure - broken url: ${page.url}, referer: ${page.referer || 'undefined'}, status code: ${page.status || 'undefined'}`);
-        if(page.url === options.url) {
-          console.error(`failure - failed on starting url: Are you sure that the site is running at "${options.url}"?`)
+        console.error(`failure - broken url: ${page.url}, referer: ${page.referer || 'undefined'}, status code: ${page.status || 'undefined'}`); // eslint-disable-line no-console
+
+        if (page.url === options.url) {
+          console.error(`failure - failed on starting url: Are you sure that the site is running at "${options.url}"?`); // eslint-disable-line no-console
+
         }
         errored.push(page);
       },
       finished: function () {
-        if (errored.length > 0) { return reject({succeded, errored}) }
-        return resolve({succeded, errored})
+        if (errored.length > 0) { return reject({succeded, errored}); }
+        return resolve({succeded, errored});
       }
     });
   });
--- packages/hexo-swagger-to-md/index.js
+++ packages/hexo-swagger-to-md/index.js
@@ -1,10 +1,14 @@
+'use strict';
+
+/* global hexo */
+
 const path = require('path');
 const fs = require('fs');
 const converter = require('widdershins');
 const validator = require('swagger-parser');
 const yaml = require('js-yaml');
 
-function swaggerToMarkdown(filepath, options) {
+function swaggerToMarkdown (filepath, options) {
   const specFileContent = fs.readFileSync(filepath,'utf8');
   const ext = path.extname(filepath);
   return validator
@@ -15,13 +19,13 @@ function swaggerToMarkdown(filepath, options) {
     });
 }
 
-function markdownToHTML(string) {
+function markdownToHTML (string) {
   return hexo.render.render({text: string, engine: 'markdown'}).then((html) => {
     return `<div class="hexo-swagger-to-md">${html}</div>`;
-  })
+  });
 }
 
-hexo.extend.tag.register('swagger_to_md', function(args){
+hexo.extend.tag.register('swagger_to_md', function (args){
   const ctx = this;
   const swagger_path = path.resolve(path.dirname(ctx.full_source), args[0]);
   return swaggerToMarkdown(swagger_path, { header: false, single: false})
