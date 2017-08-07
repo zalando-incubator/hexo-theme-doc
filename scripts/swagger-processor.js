d0230c459f04fa2014c33caa5b74305ae4517830
bmelkani <bhaskar.melkani@zalando.de>
2017-08-07 15:27:31 +0200

    chore(swagger-to-md): Make templating engine configurable

---
+++ scripts/swagger-processor.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/* global hexo */
+
+const path = require('path');
+const transformer = require('../lib/nodejs/swagger-processor');
+
+class SwaggerProcessor{
+
+  /*
+   * Default templating enging is 'html'.
+   */
+
+  constructor (engine = 'html'){
+    const availableEngines = ['md', 'html'];
+    this.engine = null;
+
+    if (availableEngines.includes(engine)){
+      this.engine = engine;
+    } else {
+      throw new TypeError(`Templating Engine(${engine}) is not supported.`);
+    }
+  }
+
+  get processor (){
+
+    const engine = this.engine;
+
+    return function (args){
+      const ctx = this;
+      const specificationPath = path.resolve(path.dirname(ctx.full_source), args[0]);
+
+      let output = '';
+      const transformerPromise = new Promise((resolve, reject) => {
+        transformer(specificationPath)
+          .on('data', (chunk) => {
+            output += chunk;
+          })
+          .on('end', () => {
+            resolve(output);
+          })
+          .on('error', (err) => {
+            reject(err);
+          });
+      });
+
+      return transformerPromise.then((output) => {
+        return hexo.render.render({text: output.toString(), engine: engine })
+          .then((html) => `<div class="swagger-processor swagger-processor-${this.engine}">${html}</div>`);
+      });
+    };
+  }
+}
+
+
+hexo.extend.tag.register('swagger_to_html', new SwaggerProcessor('md').processor, {async: true});
+
