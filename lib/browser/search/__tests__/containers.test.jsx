48b85294a2b7da784c127bd9c977accd4013f08b
Ruben Barilani <ruben.barilani@zalando.de>
2017-10-20 01:05:34 +0200

    chore(react): update to react 16

--- lib/browser/search/__tests__/containers.test.jsx
+++ lib/browser/search/__tests__/containers.test.jsx
@@ -17,29 +17,29 @@ describe('browser.search.containers', () => {
 
     it('in initial state it should not be visible', () => {
       const results = shallow(<SearchResults />);
-      expect(results.getNode()).toBe(null);
+      expect(results.getElement()).toBe(null);
     });
 
     it('when SHOW_SEARCH_RESULTS action is triggered, should be visible', () => {
       const results = mount(<SearchResults />);
       dispatch(SHOW_SEARCH_RESULTS, { results: [] });
-      expect(results.getNode()).not.toBe(null);
+      expect(results.getElement()).not.toBe(null);
     });
 
     it('when SHOW_SEARCH_RESULTS action is triggered, it should hide page content', () => {
       const results = mount(<SearchResults />);
-      expect(results.node.$page.css('display')).toBe('block');
+      expect(results.instance().$page.css('display')).toBe('block');
       dispatch(SHOW_SEARCH_RESULTS, { results: [] });
-      expect(results.node.$page.css('display')).toBe('none');
+      expect(results.instance().$page.css('display')).toBe('none');
     });
 
     it('when HIDE_SEARCH_RESULTS action is triggered, it should not be visible and the page content is visible', () => {
       const results = mount(<SearchResults />);
       dispatch(SHOW_SEARCH_RESULTS, { results: [] });
-      expect(results.node.$page.css('display')).toBe('none');
+      expect(results.instance().$page.css('display')).toBe('none');
 
       dispatch(HIDE_SEARCH_RESULTS);
-      expect(results.node.$page.css('display')).toBe('block');
+      expect(results.instance().$page.css('display')).toBe('block');
       expect(results.find('div').length).toBe(0);
     });
   });
