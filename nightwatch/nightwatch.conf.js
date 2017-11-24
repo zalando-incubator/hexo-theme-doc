3e2e0b982d69c5a2177555b367671cdb65e363c2
bhaskarmelkani <melkani.bhaskar@gmail.com>
2017-11-24 09:54:38 +0100

    Removed selemium config

--- nightwatch/nightwatch.conf.js
+++ nightwatch/nightwatch.conf.js
@@ -1,11 +1,4 @@
 module.exports = {
-  "selenium": {
-    "start_process": true,
-    "server_path": "./node_modules/webdriver-manager/selenium/selenium-server-standalone-3.7.1.jar",
-    "cli_args": {
-      "webdriver.chrome.driver": "./node_modules/webdriver-manager/selenium/chromedriver_2.33"
-    }
-  },
   "test_settings": {
     "default": {
       "launch_url": "http://localhost:4000",
